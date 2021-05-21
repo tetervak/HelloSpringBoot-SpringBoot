ca1b4e8dd05fe7b1edcd478b9d0a83d8a211a292
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-05-21 15:04:31 -0400

    used the model attribute for the name

--- src/main/java/sheridan/tetervak/hellospringboot/controller/HelloController.java
+++ src/main/java/sheridan/tetervak/hellospringboot/controller/HelloController.java
@@ -1,13 +1,17 @@
 package sheridan.tetervak.hellospringboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/Hello")
-    public String hello(){
+    public String hello(@RequestParam String name, Model model){
+
+        model.addAttribute("name", name);
         return "Hello";
     }
 
