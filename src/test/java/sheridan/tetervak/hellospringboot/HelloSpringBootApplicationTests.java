27a8c2381b21c30aea64da5e3bfb96ce0aebc313
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-05-21 13:49:18 -0400

    created the empty project

---
+++ src/test/java/sheridan/tetervak/hellospringboot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.tetervak.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
